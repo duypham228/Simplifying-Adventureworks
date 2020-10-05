91cb420638862b6ddb82db130e10f2059d316da8
wmasterson-99 <wmasterson-99@tamu.edu>
2020-10-05 14:08:55 -0500

    Added button to show all tables
    Added button to show all tables to GUIInterface. Modified a couple of variable names for more clarity

--- src/main/GUIInterface.java
+++ src/main/GUIInterface.java
@@ -35,7 +35,8 @@ public class GUIInterface extends JPanel implements MouseListener, MouseWheelLis
 	private static JFrame frame = new JFrame();
 //	private static JPanel panel;
 	private static JLabel label1;
-	private static JButton button1;
+	private static JButton findColumn;
+	private static JButton listTables;
 	private static JTextField textfield1;
 	
 	
@@ -74,10 +75,15 @@ public class GUIInterface extends JPanel implements MouseListener, MouseWheelLis
 		textfield1.setBounds(10, 50, 150, 25);
 		gui.add(textfield1);
 		
-		button1 = new JButton("jdb-find-column");
-		button1.setBounds(10, 80, 200, 25);
-		button1.addMouseListener(new GUIInterface());
-		gui.add(button1);
+		findColumn = new JButton("jdb-find-column");
+		findColumn.setBounds(10, 80, 200, 25);
+		findColumn.addMouseListener(new GUIInterface());
+		gui.add(findColumn);
+		
+		listTables = new JButton("Show List of Tables");
+		listTables.setBounds(10, 20, 200, 25);
+		listTables.addMouseListener(new GUIInterface());
+		gui.add(listTables);
 		
 		frame.setVisible(true);
 		
@@ -97,7 +103,7 @@ public class GUIInterface extends JPanel implements MouseListener, MouseWheelLis
 
 	@Override
 	public void mouseClicked(MouseEvent mouse) {
-		if (mouse.getSource() == button1) { // jdb-find-column
+		if (mouse.getSource() == findColumn) { // jdb-find-column
 //			DatabaseManager.openConnection();
 //			DatabaseManager.queryDatabase("use adventureworks;");
 			
@@ -130,6 +136,40 @@ public class GUIInterface extends JPanel implements MouseListener, MouseWheelLis
 			table1.getColumnModel().getColumn(0).setPreferredWidth(200);
 //			System.out.println(output1);
 			panel_find_column.add(sp);
+	
+			DatabaseManager.closeConnection();
+		}
+		else if (mouse.getSource() == listTables)
+		{
+			JFrame frame_list_tables = new JFrame();
+			GUIInterface panel_list_tables = new GUIInterface();
+			frame_list_tables.setVisible(true);
+			DefaultTableModel model2 = new DefaultTableModel();
+			
+			
+			JTable table2 = new JTable(model2);
+//			table1.setBounds(5, 5, 100, 300);
+			table2.setShowGrid(true);
+			table2.setGridColor(Color.black);
+			JScrollPane sp = new JScrollPane(table2);
+			sp.setPreferredSize(new Dimension(250, 300));
+			model2.addColumn("Table Name");
+//			model1.addColumn("Column");
+			
+			frame_list_tables.add(panel_list_tables);
+			panel_list_tables.setSize(300, 300);
+			
+			String output2 = DatabaseManager.handleSQLCommand("show tables;");
+			String line[] = output2.split("\n");
+			for (String token : line) {
+				token = token.substring(12, token.length() - 1);
+				String row[] = token.split(" ");
+				model2.addRow(new Object[] {row[0]});
+				
+			}
+			table2.getColumnModel().getColumn(0).setPreferredWidth(200);
+//			System.out.println(output1);
+			panel_list_tables.add(sp);
 	
 			DatabaseManager.closeConnection();
 		}
